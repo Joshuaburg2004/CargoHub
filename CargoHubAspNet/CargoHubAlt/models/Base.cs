6df45a5abf2ccfd3d27e4ec5c1a3bdb9def9a786
Joshuaburg2004 <joshuaburg2004@hotmail.nl>
2024-10-02 11:21:54 +0200

    Revert "Creation of the ASP Project"
    This reverts commit 5974f546c7c45ab0426f5b9ec01c4d5f7bbe07bb.

--- CargoHubAspNet/CargoHubAlt/models/Base.cs
+++
@@ -1,7 +0,0 @@
-using System.Globalization;
-
-public class Base{
-    public string GetTimeStamp(){
-        return DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
-    }
-}
