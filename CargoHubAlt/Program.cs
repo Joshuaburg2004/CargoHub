16e5c6690275f314bb17b02b84882215c9ad952b
Robin <robin.otterspeer2005@gmail.com>
2024-10-09 16:36:23 +0100

    program remake

--- CargoHubAlt/Program.cs
+++ CargoHubAlt/Program.cs
@@ -1,15 +1,30 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddTransient<IShipmentService, ShipmentService>();
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        app.MapControllers();
+
+        app.Urls.Add("https://localhost:5000");
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
 
 
 
-builder.Services.AddControllers();
-
-var app = builder.Build();
 
 
-app.MapControllers();
 
-app.Urls.Add("https://localhost:5000");
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
