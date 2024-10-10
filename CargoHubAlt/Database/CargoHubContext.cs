0e1e9a11cefff5f200624d21aaa0803c71fc4065
Joshuaburg2004 <joshuaburg2004@hotmail.nl>
2024-10-10 20:32:44 +0200

    CargoHubContext updated for suppliers, supplier and (I)suppliers finished up besides get items for suppliers, just need to make the controllers with all the endpoints and I'll be done again

--- CargoHubAlt/Database/CargoHubContext.cs
+++ CargoHubAlt/Database/CargoHubContext.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 
 public class CargoHubContext : DbContext{
+    public CargoHubContext(DbContextOptions<CargoHubContext> options) : base(options){}
     public DbSet<Client> Clients { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
 }
---
+++ CargoHubAlt/Interfaces/ISuppliers.cs
@@ -0,0 +1,9 @@
+public interface ISuppliers{
+    public Task<IEnumerable<Supplier>> GetAllSuppliers();
+    public Task<Supplier?> GetOneSupplier(Guid id);
+    public Task<Guid?> CreateSupplier(Supplier supplier);
+    public Task<Supplier?> DeleteSupplier(Guid id);
+    public Task<Supplier?> UpdateSupplier(Guid id, Supplier supplier);
+    // commented out because Item is not done yet
+    // public Task<IEnumerable<Item>?> GetItemsForSupplier(Guid id);
+}
