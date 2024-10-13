ac4dbd029d19d75ce3a73fcd19cd06924946bd56
robin <robin.otterspeer2005@gmail.com>
2024-10-13 12:09:29 +0200

    added shipment service

--- CargoHubAlt/Services/ShipmentService.cs
+++ CargoHubAlt/Services/ShipmentService.cs
@@ -4,28 +4,42 @@ using Microsoft.EntityFrameworkCore;
 
 public class ShipmentService : IShipmentService
 {
-    public async Task<List<Shipment>> GetShipments()
+    private readonly CargoHubContext _context;
+
+    public ShipmentService(CargoHubContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
     }
-    public async Task<Shipment> GetShipment(Guid id)
-    {
-        throw new NotImplementedException();
-    }
+
+    public async Task<List<Shipment>> GetShipments() => await _context.Shipments.ToListAsync();
+    public async Task<Shipment> GetShipment(Guid id) => await _context.Shipments.FirstOrDefaultAsync(x => x.id == id);
     public async Task<bool> AddShipment(Shipment shipment)
     {
-        throw new NotImplementedException();
+        await _context.Shipments.AddAsync(shipment);
+        await _context.SaveChangesAsync();
+        return true;
     }
     public async Task<bool> UpdateShipment(Shipment shipment)
     {
-        throw new NotImplementedException();
+        _context.Shipments.Update(shipment);
+        await _context.SaveChangesAsync();
+        return true;
     }
     public async Task<bool> Update_items_in_Shipment(Guid id, Guid shipmentid, List<Item> items)
     {
-        throw new NotImplementedException();
+        var shipment = await _context.Shipments.FirstOrDefaultAsync(x => x.id == shipmentid);
+        if (shipment == null) return false;
+        shipment.items = items;
+        _context.Shipments.Update(shipment);
+        await _context.SaveChangesAsync();
+        return true;
     }
     public async Task<bool> DeleteShipment(Guid id)
     {
-        throw new NotImplementedException();
+        var shipment = await _context.Shipments.FirstOrDefaultAsync(x => x.id == id);
+        if (shipment == null) return false;
+        _context.Shipments.Remove(shipment);
+        await _context.SaveChangesAsync();
+        return true;
     }
 }
