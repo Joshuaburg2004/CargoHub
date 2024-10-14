00f855e163e060a18ad60eb84244a6a029958b6d
Henri van der Stelt <hj.vdstelt@gmail.com>
2024-10-14 09:11:04 +0200

    item context

--- CargoHubAlt/Database/CargoHubContext.cs
+++ CargoHubAlt/Database/CargoHubContext.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 
-public class CargoHubContext : DbContext{
+public class CargoHubContext : DbContext
+{
     public DbSet<Client> Clients { get; set; }
+    public DbSet<Item> Items { get; set; }
 }
