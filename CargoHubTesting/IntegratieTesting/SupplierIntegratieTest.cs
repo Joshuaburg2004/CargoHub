4ee4ce1c787c47444a7ba7a759d22af8cca51d61
robin <robin.otterspeer2005@gmail.com>
2024-10-16 22:52:28 +0200

    databse now actually has data only guid changes to much

--- CargoHubTesting/IntegratieTesting/SupplierIntegratieTest.cs
+++ CargoHubTesting/IntegratieTesting/SupplierIntegratieTest.cs
@@ -13,82 +13,102 @@ namespace MyTests
 {
     [TestCaseOrderer("MyTests.PriorityOrderer", "CargoHubTesting")]
 
-    public class SupplierIntegratieTest : IClassFixture<WebApplicationFactory<Program>>
+    public class SupplierIntegratieTest : IClassFixture<CustomWebApplicationFactory>
     {
         private readonly HttpClient _client;
-        private readonly WebApplicationFactory<Program> _factory;
+        private readonly CustomWebApplicationFactory _factory;
         public static Guid supplier1Id;
         public static Supplier supplierafterupdate;
 
-
-        public SupplierIntegratieTest(WebApplicationFactory<Program> factory)
+        public SupplierIntegratieTest(CustomWebApplicationFactory factory)
         {
             _factory = factory;
             _client = _factory.CreateClient();
-
-            using (var scope = _factory.Services.CreateScope())
-            {
-                var dbContext = scope.ServiceProvider.GetRequiredService<CargoHubContext>();
-                dbContext.Database.Migrate();
-            }
         }
 
         [Fact, TestPriority(0)]
         public async Task GetAllSuppliersEmpty()
         {
-            Console.Error.WriteLine("GetAllSuppliersEmpty");
             HttpResponseMessage response = await _client.GetAsync("/api/v1/suppliers/getall");
             Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            // does not work because the create happens before the getall
+            Xunit.Assert.Equal("[]", await response.Content.ReadAsStringAsync());
         }
 
         [Fact, TestPriority(1)]
         public async Task CreateSupplier()
         {
-            Console.Error.WriteLine("CreateSupplier");
             HttpResponseMessage response = await _client.PostAsync("/api/v1/suppliers", new StringContent("{\"Code\":\"SUP123\",\"Name\":\"Supplier Name\",\"Address\":\"123 Main St\",\"AddressExtra\":\"Suite 100\",\"City\":\"Rotterdam\",\"ZipCode\":\"3011AA\",\"Province\":\"South Holland\",\"Country\":\"Netherlands\",\"ContactName\":\"John Doe\",\"PhoneNumber\":\"+31 10 123 4567\",\"Reference\":\"REF123\",\"CreatedAt\":\"2023-10-01T12:00:00Z\",\"UpdatedAt\":\"2023-10-01T12:00:00Z\"}", System.Text.Encoding.UTF8, "application/json"));
-            supplier1Id = JsonSerializer.Deserialize<Guid>(response.Content.ReadAsStringAsync().Result);
             Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var supplierid = JsonSerializer.Deserialize<Guid>(responseContent);
+
+            Xunit.Assert.NotNull(supplierid);
+            Xunit.Assert.NotEqual(Guid.Empty, supplierid);
+            supplier1Id = supplierid;
         }
 
         [Fact, TestPriority(2)]
         public async Task GetAllSuppliers()
         {
-            Console.Error.WriteLine("GetAllSuppliers");
+            Console.Error.WriteLine("supplier1Id: " + supplier1Id);
             HttpResponseMessage response = await _client.GetAsync("/api/v1/suppliers/getall");
             Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Xunit.Assert.NotEqual("[]", responseContent);
         }
 
         [Fact, TestPriority(3)]
         public async Task GetSupplierById()
         {
-            Console.Error.WriteLine("GetSupplierById");
             HttpResponseMessage response = await _client.GetAsync($"/api/v1/suppliers?id={supplier1Id}");
             Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var supplier = JsonSerializer.Deserialize<Supplier>(responseContent);
+            Xunit.Assert.NotNull(supplier);
+            Xunit.Assert.Equal(supplier1Id, supplier.Id);
         }
 
         [Fact, TestPriority(4)]
         public async Task UpdateSupplier()
         {
-            HttpResponseMessage response = await _client.PutAsync($"/api/v1/suppliers?id={supplier1Id}", new StringContent("{\"Code\":\"SUP12345\",\"Name\":\"Supplier Name\",\"Address\":\"123 Main St\",\"AddressExtra\":\"Suite 100\",\"City\":\"Rotterdam\",\"ZipCode\":\"3011AA\",\"Province\":\"South Holland\",\"Country\":\"Netherlands\",\"ContactName\":\"John Doe\",\"PhoneNumber\":\"+31 10 123 4567\",\"Reference\":\"REF123\",\"CreatedAt\":\"2023-10-01T12:00:00Z\",\"UpdatedAt\":\"2023-10-01T12:00:00Z\"}", System.Text.Encoding.UTF8, "application/json"));
+            HttpResponseMessage response = await _client.PutAsync($"/api/v1/suppliers?id={supplier1Id}", new StringContent("{\"Code\":\"SUP12345\",\"Name\":\"Supplier Name Updated\",\"Address\":\"123 Main St\",\"AddressExtra\":\"Suite 100\",\"City\":\"Rotterdam\",\"ZipCode\":\"3011AA\",\"Province\":\"South Holland\",\"Country\":\"Netherlands\",\"ContactName\":\"John Doe\",\"PhoneNumber\":\"+31 10 123 4567\",\"Reference\":\"REF123\",\"CreatedAt\":\"2023-10-01T12:00:00Z\",\"UpdatedAt\":\"2023-10-01T12:00:00Z\"}", System.Text.Encoding.UTF8, "application/json"));
             Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
             var responseContent = await response.Content.ReadAsStringAsync();
-            Console.Error.WriteLine(responseContent);
-
             supplierafterupdate = JsonSerializer.Deserialize<Supplier>(responseContent);
-            Console.Error.WriteLine(supplierafterupdate.Id);
 
             Xunit.Assert.NotNull(supplierafterupdate);
             Xunit.Assert.NotEqual(Guid.Empty, supplierafterupdate.Id);
+            Xunit.Assert.Equal("Supplier Name Updated", supplierafterupdate.Name);
         }
 
         [Fact, TestPriority(5)]
+        public async Task GetSupplierByIdAfterUpdate()
+        {
+            HttpResponseMessage response = await _client.GetAsync($"/api/v1/suppliers?id={supplierafterupdate.Id}");
+            Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var supplier = JsonSerializer.Deserialize<Supplier>(responseContent);
+            Xunit.Assert.NotNull(supplier);
+            Xunit.Assert.Equal(supplierafterupdate.Id, supplier.Id);
+            Xunit.Assert.Equal("Supplier Name Updated", supplier.Name);
+        }
+
+        [Fact, TestPriority(6)]
         public async Task DeleteSupplier()
         {
-            Console.Error.WriteLine("DeleteSupplier");
             HttpResponseMessage response = await _client.DeleteAsync($"/api/v1/suppliers?id={supplierafterupdate.Id}");
             Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact, TestPriority(7)]
+        public async Task GetAllSuppliersAfterDelete()
+        {
+            HttpResponseMessage response = await _client.GetAsync("/api/v1/suppliers/getall");
+            Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Xunit.Assert.Equal("[]", responseContent);
         }
     }
 }
