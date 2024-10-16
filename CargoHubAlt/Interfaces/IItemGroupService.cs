95f4f2f0d278b569bed9127366f96997f0971e60
NotaTree1 <rowankiksen@gmail.com>
2024-10-16 10:42:51 +0200

    save interface and service of item group

--- CargoHubAlt/Interfaces/IItemGroupService.cs
+++ CargoHubAlt/Interfaces/IItemGroupService.cs
@@ -1,9 +1,9 @@
 public interface IItemGroupService
 {
-    public Task<Item_group> FindItemGroup(int Id);
-    public Task<IEnumerable<Item_group>> FindManyItemGroup(IEnumerable<int> Ids);
+    public Task<Item_group?> FindItemGroup(int Id);
+    public Task<IEnumerable<Item_group?>> FindManyItemGroup(IEnumerable<int> Ids);
     public Task<IEnumerable<Item_group>> GetAllItemGroup();
     public Task<bool> AddItemGroup(Item_group toAdd);
-    public Task<Item_group> UpdateItemGroup(int Id, Item_group toUpdate);
-    public Task<Item_group> DeleteItemGroup(int Id);
+    public Task<Item_group?> UpdateItemGroup(int Id, Item_group toUpdate);
+    public Task<Item_group?> DeleteItemGroup(int Id);
 }
--- CargoHubAlt/Services/ItemGroupService.cs
+++ CargoHubAlt/Services/ItemGroupService.cs
@@ -1,10 +1,59 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+
 public class ItemGroupService: IItemGroupService
 {
     private readonly CargoHubContext _cargoHubContext;
+
+
     public ItemGroupService(CargoHubContext context){
         _cargoHubContext = context;
     }
 
+    public async Task<Item_group?> FindItemGroup(int Id)
+    {
+        if (Id < 0) return null;
+        return await this._cargoHubContext.Item_Groups.FirstOrDefaultAsync(item_group => item_group.Id == Id);    }
+    public async Task<IEnumerable<Item_group?>> FindManyItemGroup(IEnumerable<int> Ids)
+    {
+        List<Item_group?> toReturn = new List<Item_group?>();
+        
+        foreach (int id in Ids)
+        {
+            if (id < 0) toReturn.Append(null);
+            else toReturn.Append(await this._cargoHubContext.Item_Groups.FirstOrDefaultAsync(item_group => item_group.Id == id));
+        }
+        return toReturn;
+
+    }
+    public async Task<IEnumerable<Item_group>> GetAllItemGroup()
+    {
+        return await this._cargoHubContext.Item_Groups.ToListAsync();
+    }
+    public async Task<bool> AddItemGroup(Item_group toAdd)
+    {
+        await this._cargoHubContext.Item_Groups.AddAsync(toAdd);
+        if (await this._cargoHubContext.SaveChangesAsync() >= 1) return true;
+        else return false;
+    }
+    public async Task<Item_group?> UpdateItemGroup(int Id, Item_group toUpdate)
+    {
+        Item_group? found = await this.FindItemGroup(Id);
+        if (found is null) return null;
+        
+        this._cargoHubContext.Item_Groups.Update(toUpdate);
+        await this._cargoHubContext.SaveChangesAsync();
+        return found;
+    }
 
 
+    public async Task<Item_group?> DeleteItemGroup(int Id)
+    {
+        Item_group? found = await this.FindItemGroup(Id);
+        if (found is null) return null;
+
+        this._cargoHubContext.Item_Groups.Remove(found);
+        await this._cargoHubContext.SaveChangesAsync();
+        return found;
+    }
 }
