58a86b3b20ad4ce0d0c2e7a054108f9c07a07b93
robin <robin.otterspeer2005@gmail.com>
2024-10-17 14:43:01 +0200

    client integration

--- CargoHubAlt/Models/Locations.cs
+++ CargoHubAlt/Models/Locations.cs
@@ -1,21 +1,21 @@
-public class Location: Base{
-    
+using Microsoft.EntityFrameworkCore;
+[PrimaryKey("Id")]
+public class Location : Base
+{
+    public Guid Id { get; set; }
+    public Guid warehouse_Id { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string created_at { get; set; }
+    public string updated_at { get; set; }
 
-    public Guid Id {get; set;}
-    public Guid warehouse_Id {get; set;}
-    public string Code {get; set;}
-    public string Name {get; set;}
-    public string created_at {get; set;}
-    public string updated_at {get; set;}
-
-    public Location(Guid id, Guid warehouse_id, string code, string name, string created_at, string updated_at)
+    public Location(string code, string name, string created_at, string updated_at)
     {
-        this.Id = id;
-        this.warehouse_Id = warehouse_Id;
-        this.Code = code;
-        this.Name = name;
+        Id = new Guid();
+        warehouse_Id = new Guid();
+        Code = code;
+        Name = name;
         this.created_at = created_at;
         this.updated_at = updated_at;
     }
-
 }
