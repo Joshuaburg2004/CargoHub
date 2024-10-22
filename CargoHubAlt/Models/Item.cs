24d48f26294609de90834f7dbfaf1fc4a64a1cf7
NotaTree1 <rowankiksen@gmail.com>
2024-10-22 17:54:49 +0200

    think im done with item

--- CargoHubAlt/Models/Item.cs
+++ CargoHubAlt/Models/Item.cs
@@ -8,19 +8,19 @@ public class Item : Base
     public string UpcCode { get; set; }
     public string ModelNumber { get; set; }
     public string CommodityCode { get; set; }
-    public int ItemLine { get; set; }
-    public int ItemGroup { get; set; }
-    public int ItemType { get; set; }
+    public Guid ItemLine { get; set; }
+    public Guid ItemGroup { get; set; }
+    public Guid ItemType { get; set; }
     public int UnitPurchaseQuantity { get; set; }
     public int UnitOrderQuantity { get; set; }
     public int PackOrderQuantity { get; set; }
-    public int SupplierId { get; set; }
+    public Guid SupplierId { get; set; }
     public string SupplierCode { get; set; }
     public string SupplierPartNumber { get; set; }
     public string CreatedAt { get; set; } = GetTimeStamp();
     public string UpdatedAt { get; set; } = GetTimeStamp();
 
-    public Item(string uid, string code, string description, string shortDescription, string upcCode, string modelNumber, string commodityCode, int itemLine, int itemGroup, int itemType, int unitPurchaseQuantity, int unitOrderQuantity, int packOrderQuantity, int supplierId, string supplierCode, string supplierPartNumber)
+    public Item(string uid, string code, string description, string shortDescription, string upcCode, string modelNumber, string commodityCode, Guid itemLine, Guid itemGroup, Guid itemType, int unitPurchaseQuantity, int unitOrderQuantity, int packOrderQuantity, Guid supplierId, string supplierCode, string supplierPartNumber)
     {
         Id = Guid.NewGuid();
         Uid = uid;
