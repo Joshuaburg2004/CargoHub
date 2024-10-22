ef9d2b9e64f001f3b652fea1bcd54e5c166586c7
Robin <robin.otterspeer2005@gmail.com>
2024-10-22 17:50:10 +0100

    item type controller service and interface

--- CargoHubAlt/Database/CargoHubContext.cs
+++ CargoHubAlt/Database/CargoHubContext.cs
@@ -10,9 +10,10 @@ public class CargoHubContext : DbContext
     public DbSet<Shipment> Shipments { get; set; }
     public DbSet<Transfer> Transfers { get; set; }
     public DbSet<Item> Items { get; set; }
-    public DbSet<Location> Locations {get; set;}
-    public DbSet<Item_group> Item_Groups {get; set;}
-    public DbSet<Item_line> Item_Lines {get; set;}
-    public DbSet<Inventory> Inventories {get; set;}
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Item_group> Item_Groups { get; set; }
+    public DbSet<Item_line> Item_Lines { get; set; }
+    public DbSet<Inventory> Inventories { get; set; }
     public DbSet<Order> Orders { get; set; }
+    public DbSet<Item_type> Item_Types { get; set; }
 }
