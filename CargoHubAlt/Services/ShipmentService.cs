d598c0fd4cc59297e019827f71cc871b4b9c05b7
Robin <robin.otterspeer2005@gmail.com>
2024-10-22 10:13:52 +0100

    fix update shipments

--- CargoHubAlt/Services/ShipmentService.cs
+++ CargoHubAlt/Services/ShipmentService.cs
@@ -21,7 +21,29 @@ public class ShipmentService : IShipmentService
     }
     public async Task<bool> UpdateShipment(Shipment shipment)
     {
-        _context.Shipments.Update(shipment);
+        Shipment oldshipment = await _context.Shipments.FirstOrDefaultAsync(x => x.Id == shipment.Id);
+        if (oldshipment == null) return false;
+
+        oldshipment.Id = shipment.Id;
+        oldshipment.Order_id = shipment.Order_id;
+        oldshipment.Source_id = shipment.Source_id;
+        oldshipment.Order_date = shipment.Order_date;
+        oldshipment.Request_date = shipment.Request_date;
+        oldshipment.Shipment_date = shipment.Shipment_date;
+        oldshipment.Shipment_type = shipment.Shipment_type;
+        oldshipment.Shipment_status = shipment.Shipment_status;
+        oldshipment.Notes = shipment.Notes;
+        oldshipment.Carrier_code = shipment.Carrier_code;
+        oldshipment.Carrier_description = shipment.Carrier_description;
+        oldshipment.Service_code = shipment.Service_code;
+        oldshipment.Payment_type = shipment.Payment_type;
+        oldshipment.Transfer_mode = shipment.Transfer_mode;
+        oldshipment.Total_package_count = shipment.Total_package_count;
+        oldshipment.Total_package_weight = shipment.Total_package_weight;
+        oldshipment.Updated_at = shipment.Updated_at;
+        oldshipment.Items = shipment.Items;
+
+        _context.Shipments.Update(oldshipment);
         await _context.SaveChangesAsync();
         return true;
     }
