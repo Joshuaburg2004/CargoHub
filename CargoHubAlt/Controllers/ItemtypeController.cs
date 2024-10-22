4563954d3cbe25a26f226d386df9a09eb705c12d
Joshuaburg2004 <joshuaburg2004@hotmail.nl>
2024-10-22 21:04:08 +0200

    Item_type testing done

--- CargoHubAlt/Controllers/ItemtypeController.cs
+++ CargoHubAlt/Controllers/ItemtypeController.cs
@@ -17,30 +17,32 @@ public class ItemTypeController : Controller
     }
 
     [HttpGet("getbyid")]
-    public async Task<IActionResult> GetItemLineById([FromQuery] Guid id)
+    public async Task<IActionResult> GetItemTypeById([FromQuery] Guid id)
     {
         if (id == Guid.Empty) return BadRequest("ID is empty");
-        return Ok(await _itemsService.GetItemLineById(id));
+        var item_type = await _itemsService.GetItemTypeById(id);
+        if (item_type is null) return NotFound("Item Type not found");
+        else return Ok(item_type);
     }
 
-    [HttpPost("add")]
-    public async Task<IActionResult> AddItemLine([FromBody] Item_type? itemtype)
+    [HttpPost()]
+    public async Task<IActionResult> AddItemType([FromBody] Item_type? itemtype)
     {
-        if (itemtype is null) return BadRequest("this is not an item Line");
+        if (itemtype is null) return BadRequest("this is not an item Type");
         Guid? success = await _itemsService.AddItemType(itemtype);
-        if (success is null) return BadRequest("Item Line not added");
+        if (success is null) return BadRequest("Item Type not added");
         else return Ok(success);
     }
 
-    [HttpPut("update")]
-    public async Task<IActionResult> putItemLine([FromQuery] Guid id, [FromBody] Item_type itemtype)
+    [HttpPut()]
+    public async Task<IActionResult> putItemType([FromQuery] Guid id, [FromBody] Item_type itemtype)
     {
         if (id == Guid.Empty) return BadRequest("ID is empty");
         return Ok(await _itemsService.UpdateItemType(id, itemtype));
     }
 
-    [HttpDelete("delete")]
-    public async Task<IActionResult> deleteItemLine([FromQuery] Guid id)
+    [HttpDelete()]
+    public async Task<IActionResult> deleteItemType([FromQuery] Guid id)
     {
         if (id == Guid.Empty) return BadRequest("ID is empty");
         return Ok(await _itemsService.DeleteItemType(id));
