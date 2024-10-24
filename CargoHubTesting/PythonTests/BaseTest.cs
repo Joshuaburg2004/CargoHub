396e051b120972c4df2563c4f43fcc5323989551
Joshuaburg2004 <joshuaburg2004@hotmail.nl>
2024-10-24 14:31:53 +0200

    TestOrder not working for some reason, create is running before get

--- CargoHubTesting/PythonTests/BaseTest.cs
+++ CargoHubTesting/PythonTests/BaseTest.cs
@@ -9,20 +9,4 @@ public class BaseTest
         _client.BaseAddress = new Uri("http://localhost:3000");
         _client.DefaultRequestHeaders.Add("API_KEY", "a1b2c3d4e5");
     }
-
-    [Fact]
-    public async Task TestGetClients()
-    {
-        // Arrange
-        var requestUri = "/api/v1/clients";
-
-        // Act
-        var response = await _client.GetAsync(requestUri);
-
-        // Assert
-        response.EnsureSuccessStatusCode();
-        var responseContent = await response.Content.ReadAsStringAsync();
-        Xunit.Assert.NotNull(responseContent);
-        // Additional assertions based on expected response content
-    }
 }
--- CargoHubTesting/PythonTests/LocationTest.cs
+++ CargoHubTesting/PythonTests/LocationTest.cs
@@ -4,27 +4,29 @@ using PythonTests;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Xunit;
-[TestCaseOrderer("MyTests.PriorityOrderer", "PythonTesting")]
-public class LocationTest : BaseTest {
-    public LocationTest() : base(){}
-    [Fact, TestPriority(0)]
-    public async Task GetAllLocations()
-    {
-        var requestUri = "/api/v1/locations";
-        var response = await _client.GetAsync(requestUri);
-        var result = await response.Content.ReadAsStringAsync();
-        Xunit.Assert.NotNull(result);
-        Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        Xunit.Assert.Equal("[]", result);
-    }
-    [Fact, TestPriority(1)]
-    public async Task CreateLocation()
-    {
-        var requestUri = "/api/v1/locations";
-        var response = await _client.PostAsync(requestUri, new StringContent("{\"id\": 1, \"warehouse_id\": 1, \"code\": \"A.1.0\", \"name\": \"Row: A, Rack: 1, Shelf: 0\", \"created_at\": \"1992-05-15 03:21:32\", \"updated_at\": \"1992-05-15 03:21:32\"}"));
-        var result = await response.Content.ReadAsStringAsync();
-        Xunit.Assert.NotNull(result);
-        Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        Xunit.Assert.Equal("[]", result);
+namespace PythonTests{
+    [TestCaseOrderer("MyTests.PriorityOrderer", "PythonTesting")]
+    public class LocationTest : BaseTest {
+        public LocationTest() : base(){}
+        [Fact]
+        [TestPriority(0)]
+        public async Task GetAllLocations()
+        {
+            var requestUri = "/api/v1/locations";
+            var response = await _client.GetAsync(requestUri);
+            var result = await response.Content.ReadAsStringAsync();
+            Xunit.Assert.NotNull(result);
+            Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Xunit.Assert.Equal("[]", result);
+        }
+        [Fact]
+        [TestPriority(1)]
+        public async Task CreateLocation()
+        {
+            var requestUri = "/api/v1/locations";
+            var response = await _client.PostAsync(requestUri, new StringContent("{\"id\": 1, \"warehouse_id\": 1, \"code\": \"A.1.0\", \"name\": \"Row: A, Rack: 1, Shelf: 0\", \"created_at\": \"1992-05-15 03:21:32\", \"updated_at\": \"1992-05-15 03:21:32\"}"));
+            var result = await response.Content.ReadAsStringAsync();
+            Xunit.Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
     }
 }
