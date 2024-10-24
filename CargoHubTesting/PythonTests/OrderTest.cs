45d23ea68afc450e759ff142aa459ca0d9661ae9
Joshuaburg2004 <joshuaburg2004@hotmail.nl>
2024-10-24 21:12:26 +0200

    Final test to check if put actually updates the items in order - it doesnt currently because inventories are missing from this test, but still

--- CargoHubTesting/PythonTests/OrderTest.cs
+++ CargoHubTesting/PythonTests/OrderTest.cs
@@ -64,6 +64,16 @@ namespace PythonTests{
             Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
         [Fact, TestPriority(7)]
+        public async Task GetOneOrderAfterPutting(){
+            var requestUri = "/api/v1/orders/1";
+            var response = await _client.GetAsync(requestUri);
+            var result = await response.Content.ReadAsStringAsync();
+            Xunit.Assert.NotNull(result);
+            Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Xunit.Assert.Contains("{\"id\": 1, \"source_id\": 52, \"order_date\": \"1983-09-26T19:06:08Z\", \"request_date\": \"1983-09-30T19:06:08Z\", \"reference\": \"ORD00003\", \"reference_extra\": \"Vergeven kamer goed enkele wiel tussen.\", \"order_status\": \"Delivered\", \"notes\": \"Zeil hoeveel onze map sex ding.\", \"shipping_notes\": \"Ontvangen schoon voorzichtig instrument ster vijver kunnen raam.\", \"picking_notes\": \"Grof geven politie suiker bodem zuid.\", \"warehouse_id\": 11, \"ship_to\": null, \"bill_to\": null, \"shipment_id\": 3, \"total_amount\": 1156.14, \"total_discount\": 420.45, \"total_tax\": 677.42, \"total_surcharge\": 86.03, \"created_at\": ",  result);
+        }
+        
+        [Fact, TestPriority(8)]
         public async Task PutOrderItems(){
             var requestUri = "/api/v1/orders/1/items";
             var response = await _client.PutAsync(requestUri, new StringContent("[{\"item_id\": \"P010689\", \"amount\": 16}]"));
@@ -73,23 +83,23 @@ namespace PythonTests{
             // Because I turned on debug within the python code, the inventories do not exist.
             // Makes sense, but it is not the expected behavior.
         }
-        [Fact, TestPriority(8)]
-        public async Task GetOneOrderAfterPutting(){
-            var requestUri = "/api/v1/orders/1";
+        [Fact, TestPriority(9)]
+        public async Task GetItemsFromOrderAfterPutting(){
+            var requestUri = "/api/v1/orders/1/items";
             var response = await _client.GetAsync(requestUri);
             var result = await response.Content.ReadAsStringAsync();
             Xunit.Assert.NotNull(result);
             Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Xunit.Assert.Contains("{\"id\": 1, \"source_id\": 52, \"order_date\": \"1983-09-26T19:06:08Z\", \"request_date\": \"1983-09-30T19:06:08Z\", \"reference\": \"ORD00003\", \"reference_extra\": \"Vergeven kamer goed enkele wiel tussen.\", \"order_status\": \"Delivered\", \"notes\": \"Zeil hoeveel onze map sex ding.\", \"shipping_notes\": \"Ontvangen schoon voorzichtig instrument ster vijver kunnen raam.\", \"picking_notes\": \"Grof geven politie suiker bodem zuid.\", \"warehouse_id\": 11, \"ship_to\": null, \"bill_to\": null, \"shipment_id\": 3, \"total_amount\": 1156.14, \"total_discount\": 420.45, \"total_tax\": 677.42, \"total_surcharge\": 86.03, \"created_at\": ",  result);
+            Xunit.Assert.Equal("[{\"item_id\": \"P010689\", \"amount\": 16}]", result);
         }
-        [Fact, TestPriority(9)]
+        [Fact, TestPriority(10)]
         public async Task DeleteOrder(){
             var requestUri = "/api/v1/orders/1";
             var response = await _client.DeleteAsync(requestUri);
             var result = await response.Content.ReadAsStringAsync();
             Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
-        [Fact, TestPriority(10)]
+        [Fact, TestPriority(11)]
         public async Task GetOneOrderAfterDelete(){
             var requestUri = "/api/v1/orders/1";
             var response = await _client.GetAsync(requestUri);
@@ -100,7 +110,7 @@ namespace PythonTests{
             // It should absolutely be either 400 bad request or 404 not found, but it is 200 OK.
             // This is idiotic.
         }
-        [Fact, TestPriority(11)]
+        [Fact, TestPriority(12)]
         public async Task GetAllOrdersAfterDelete()
         {
             var requestUri = "/api/v1/orders";
