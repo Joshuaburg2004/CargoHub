a266304a854d472529f2aab5c137d7519790d0d5
robin <robin.otterspeer2005@gmail.com>
2024-10-29 16:40:24 +0100

    controller and interface now int

--- CargoHubAlt/Controllers/TransferController.cs
+++ CargoHubAlt/Controllers/TransferController.cs
@@ -14,16 +14,16 @@ public class TransferController : Controller
     public async Task<IActionResult> GetTransfers() => Ok(await _transferservice.GetTransfers());
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetTranfersById(Guid id)
+    public async Task<IActionResult> GetTranfersById(int id)
     {
-        if (id == Guid.Empty) return BadRequest();
+        if (id <= 0) return BadRequest();
         return Ok(await _transferservice.GetTransferById(id));
     }
 
     [HttpGet("{id}/items")]
-    public async Task<IActionResult> GetItemsInTransfer(Guid id)
+    public async Task<IActionResult> GetItemsInTransfer(int id)
     {
-        if (id == Guid.Empty) return BadRequest();
+        if (id <= 0) return BadRequest();
         return Ok(await _transferservice.GetItemsInTransfer(id));
     }
 
@@ -35,16 +35,16 @@ public class TransferController : Controller
     }
 
     [HttpDelete("{id}")]
-    public async Task<IActionResult> RemoveTransfer(Guid id)
+    public async Task<IActionResult> RemoveTransfer(int id)
     {
-        if (id == Guid.Empty) return BadRequest();
+        if (id <= 0) return BadRequest();
         return Ok(await _transferservice.RemoveTransfer(id));
     }
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateTransfer(Guid id, Transfer transfer)
+    public async Task<IActionResult> UpdateTransfer(int id, Transfer transfer)
     {
-        if (id == Guid.Empty && transfer == null) return BadRequest(); // && moet of zijn maar staat niet op dit toetsenbord zal dit later aanpassen
+        if (id <= 0 && transfer == null) return BadRequest(); // && moet of zijn maar staat niet op dit toetsenbord zal dit later aanpassen
         return Ok(await _transferservice.UpdateTransfer(id, transfer));
     }
 }
--- CargoHubAlt/Interfaces/ITransfers.cs
+++ CargoHubAlt/Interfaces/ITransfers.cs
@@ -3,9 +3,9 @@ using Microsoft.AspNetCore.Http.Features;
 public interface ITransfer
 {
     public Task<List<Transfer>> GetTransfers();
-    public Task<Transfer> GetTransferById(Guid transfer_id);
-    public Task<List<Item>> GetItemsInTransfer(Guid transfer_id);
+    public Task<Transfer> GetTransferById(int transfer_id);
+    public Task<List<Item>> GetItemsInTransfer(int transfer_id);
     public Task<bool> AddTransfer(Transfer transfer);
-    public Task<bool> UpdateTransfer(Guid Transfer_id, Transfer transfer);
-    public Task<bool> RemoveTransfer(Guid transfer_id);
+    public Task<bool> UpdateTransfer(int Transfer_id, Transfer transfer);
+    public Task<bool> RemoveTransfer(int transfer_id);
 }
