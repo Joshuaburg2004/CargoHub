ea1450d1c3504c257e3092c6706755351469d166
Henri van der Stelt <hj.vdstelt@gmail.com>
2024-10-29 16:08:27 +0100

    Orders rewrite done

--- CargoHubAlt/Interfaces/IOrderService.cs
+++ CargoHubAlt/Interfaces/IOrderService.cs
@@ -1,8 +1,8 @@
 public interface IOrderService
 {
-    public Task<List<Order>> GetOrders();
-    public Task<Order> GetOrder(int orderId);
-    public Task<List<OrderedItem>> GetOrderedItems(int orderId);
+    public Task<List<Order>?> GetOrders();
+    public Task<Order?> GetOrder(int orderId);
+    public Task<List<OrderedItem>?> GetOrderedItems(int orderId);
     public Task<bool> AddOrder(Order order);
     public Task<bool> UpdateOrder(Order order);
     public Task<bool> UpdateOrderedItems(int orderId, List<OrderedItem> items);
