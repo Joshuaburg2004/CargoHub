59a48ef7273b68f66122953ce1ce3464b6941c03
robin <robin.otterspeer2005@gmail.com>
2024-10-30 13:39:46 +0100

    controller now has from body and route

--- CargoHubAlt/Controllers/TransferController.cs
+++ CargoHubAlt/Controllers/TransferController.cs
@@ -14,35 +14,35 @@ public class TransferController : Controller
     public async Task<IActionResult> GetTransfers() => Ok(await _transferservice.GetTransfers());
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetTranfersById(int id)
+    public async Task<IActionResult> GetTranfersById([FromRoute] int id)
     {
         if (id <= 0) return BadRequest();
         return Ok(await _transferservice.GetTransferById(id));
     }
 
     [HttpGet("{id}/items")]
-    public async Task<IActionResult> GetItemsInTransfer(int id)
+    public async Task<IActionResult> GetItemsInTransfer([FromRoute] int id)
     {
         if (id <= 0) return BadRequest();
         return Ok(await _transferservice.GetItemsInTransfer(id));
     }
 
     [HttpPost()]
-    public async Task<IActionResult> AddTransfer(Transfer transfer)
+    public async Task<IActionResult> AddTransfer([FromBody] Transfer transfer)
     {
         if (transfer == null || transfer.Items == null) return BadRequest();
         return Ok(await _transferservice.AddTransfer(transfer));
     }
 
     [HttpDelete("{id}")]
-    public async Task<IActionResult> RemoveTransfer(int id)
+    public async Task<IActionResult> RemoveTransfer([FromRoute] int id)
     {
         if (id <= 0) return BadRequest();
         return Ok(await _transferservice.RemoveTransfer(id));
     }
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateTransfer(int id, Transfer transfer)
+    public async Task<IActionResult> UpdateTransfer([FromRoute] int id, [FromBody] Transfer transfer)
     {
         if (id <= 0 && transfer == null) return BadRequest(); // && moet of zijn maar staat niet op dit toetsenbord zal dit later aanpassen
         return Ok(await _transferservice.UpdateTransfer(id, transfer));
