8d199a56a968b5241ceb9e912dbd87ea6dc6c295
Joshuaburg2004 <joshuaburg2004@hotmail.nl>
2024-10-31 11:07:33 +0100

    Emptied out and renamed v1 controller and deleted v2 controller

--- CargoHubAlt/Controllers/Clientv1Controller.cs
+++ CargoHubAlt/Controllers/Clientv1Controller.cs
@@ -8,17 +8,6 @@ public class ClientV1Controller : Controller
     {
         Clients = clients;
     }
-    [HttpGet("getall")]
-    public async Task<IActionResult> GetAllClients() => Ok(await Clients.GetAllClients());
-    [HttpGet()]
-    public async Task<IActionResult> GetOneClient([FromQuery] Guid id) => Ok(await Clients.GetClient(id));
-    [HttpPost()]
-    public async Task<IActionResult> AddClient([FromBody] Client client) => Created("Client created!", await Clients.AddClient(client));
-    [HttpPut()]
-    public async Task<IActionResult> UpdateClient([FromQuery] Guid guid, [FromBody] Client client) => Ok(await Clients.UpdateClient(guid, client));
-    [HttpDelete()]
-    public async Task<IActionResult> DeleteClient([FromQuery] Guid guid) => Ok(await Clients.RemoveClient(guid));
-    [HttpGet("{id}/orders")]
-    public async Task<IActionResult> GetOrders(Guid ordersId) => throw new NotImplementedException();
+    
 
 }
