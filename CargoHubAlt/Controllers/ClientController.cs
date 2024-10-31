db0fa2744ffba9a4f24e567038d765ea0525a5d5
Joshuaburg2004 <joshuaburg2004@hotmail.nl>
2024-10-31 11:35:06 +0100

    Actually finished the Post, Delete and Put for client

--- CargoHubAlt/Controllers/ClientController.cs
+++ CargoHubAlt/Controllers/ClientController.cs
@@ -34,4 +34,31 @@ public class ClientController : Controller
         }
         return Ok(orders);
     }
+    [HttpPost("clients")]
+    public async Task<IActionResult> AddClient([FromBody] Client client){
+        if(client == null){
+            return BadRequest("Client cannot be null.");
+        }
+        await Clients.AddClient(client);
+        return Created();
+    }
+    [HttpPut("clients/{id}")]
+    public async Task<IActionResult> UpdateClient([FromRoute] int id, [FromBody] Client client){
+        if(id < 0){
+            return BadRequest("ID cannot be smaller than 0.");
+        }
+        if(client == null){
+            return BadRequest("Client cannot be null.");
+        }
+        await Clients.UpdateClient(id, client);
+        return Ok();
+    }
+    [HttpDelete("clients/{id}")]
+    public async Task<IActionResult> DeleteClient([FromRoute] int id){
+        if(id < 0){
+            return BadRequest("ID cannot be smaller than 0.");
+        }
+        await Clients.RemoveClient(id);
+        return Ok();
+    }
 }
