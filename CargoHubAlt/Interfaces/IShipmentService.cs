a2a0a3d204b5f431f7c81e39179923fae5133c29
robin <robin.otterspeer2005@gmail.com>
2024-11-11 13:52:38 +0100

    interfaces now namespace

--- CargoHubAlt/Interfaces/IShipmentService.cs
+++ CargoHubAlt/Interfaces/IShipmentService.cs
@@ -1,9 +1,14 @@
-public interface IShipmentService
+using CargoHubAlt.Models;
+
+namespace CargoHubAlt.Interfaces
 {
-    Task<List<Shipment>?> GetShipments();
-    Task<Shipment?> GetShipment(int id);
-    Task<bool> AddShipment(Shipment shipment);
-    Task<bool> UpdateShipment(Shipment shipment);
-    Task<bool> Update_items_in_Shipment(int id, int shipmentid, List<ShipmentItem> items);
-    Task<bool> DeleteShipment(int id);
+    public interface IShipmentService
+    {
+        public Task<Shipment?> GetShipment(int Id);
+        public Task<IEnumerable<Shipment>> GetAllShipment();
+        public Task<IEnumerable<ShipmentItem>?> GetItemsfromShipmentById(int Id);
+        public Task<int?> AddShipment(Shipment shipment);
+        public Task<Shipment?> UpdateShipment(int Id, Shipment shipment);
+        public Task<Shipment?> DeleteShipment(int Id);
+    }
 }
