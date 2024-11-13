52ac3ade681addfd22fb76008aa9a2213098cd47
Robin <robin.otterspeer2005@gmail.com>
2024-11-13 15:20:45 +0100

    supplier now uses model and working

--- IntegrationTests/SupplierTest.cs
+++ IntegrationTests/SupplierTest.cs
@@ -5,11 +5,15 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Xunit;
 using System.Text;
+using CargoHubAlt.Models;
+using System.Net.Http.Json;
 namespace IntegrationTests
 {
     [TestCaseOrderer("IntegrationTests.PriorityOrderer", "IntegrationTests")]
     public class SupplierTest : BaseTest
     {
+        private Supplier _newsupplier = new Supplier(1, "SUP0001", "Lee, Parks and Johnson", "5989 Sullivan Drives", "Apt. 996", "Port Anitaburgh", "91688", "Illinois", "Czech Republic", "Toni Barnett", "363.541.7282x36825", "LPaJ-SUP0001");
+        private Supplier _supplierToPut = new Supplier(1, "SUP0001", "Bob, Parks and Johnson", "5989 Sullivan Drives", "Apt. 996", "Port Anitaburgh", "91688", "Illinois", "Czech Republic", "Toni Barnett", "363.541.7282x36825", "LPaJ-SUP0001");
         public SupplierTest(CustomWebApplicationFactory<Program> factory) : base(factory) { }
         [Fact, TestPriority(1)]
         public async Task GetAllSuppliers()
@@ -36,7 +40,7 @@ namespace IntegrationTests
         public async Task CreateSupplier()
         {
             var requestUri = "/api/v1/suppliers";
-            var response = await _client.PostAsync(requestUri, new StringContent("{\"id\": 1, \"code\": \"SUP0001\", \"name\": \"Lee, Parks and Johnson\", \"address\": \"5989 Sullivan Drives\", \"address_extra\": \"Apt. 996\", \"city\": \"Port Anitaburgh\", \"zip_code\": \"91688\", \"province\": \"Illinois\", \"country\": \"Czech Republic\", \"contact_name\": \"Toni Barnett\", \"phonenumber\": \"363.541.7282x36825\", \"reference\": \"LPaJ-SUP0001\", \"created_at\": \"1971-10-20 18:06:17\", \"updated_at\": \"1985-06-08 00:13:46\"}", Encoding.UTF8, "application/json"));
+            var response = await _client.PostAsJsonAsync(requestUri, _newsupplier);
             var result = await response.Content.ReadAsStringAsync();
             Xunit.Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         }
@@ -48,13 +52,28 @@ namespace IntegrationTests
             var result = await response.Content.ReadAsStringAsync();
             Xunit.Assert.NotNull(result);
             Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Xunit.Assert.Contains("{\"id\":1,\"code\":\"SUP0001\",\"name\":\"Lee, Parks and Johnson\",\"address\":\"5989 Sullivan Drives\",\"address_Extra\":\"Apt. 996\",\"city\":\"Port Anitaburgh\",\"zip_Code\":\"91688\",\"province\":\"Illinois\",\"country\":\"Czech Republic\",\"contact_Name\":\"Toni Barnett\",\"phoneNumber\":\"363.541.7282x36825\",\"reference\":\"LPaJ-SUP0001\"", result);
-        }
+            Supplier? supplier = await response.Content.ReadFromJsonAsync<Supplier>();
+            
+            Xunit.Assert.NotNull(supplier);
+            Xunit.Assert.Equal(_newsupplier.Id, supplier.Id);
+            Xunit.Assert.Equal(_newsupplier.Code, supplier.Code);
+            Xunit.Assert.Equal(_newsupplier.Name, supplier.Name);
+            Xunit.Assert.Equal(_newsupplier.Address, supplier.Address);
+            Xunit.Assert.Equal(_newsupplier.AddressExtra, supplier.AddressExtra);
+            Xunit.Assert.Equal(_newsupplier.City, supplier.City);
+            Xunit.Assert.Equal(_newsupplier.ZipCode, supplier.ZipCode);
+            Xunit.Assert.Equal(_newsupplier.Province, supplier.Province);
+            Xunit.Assert.Equal(_newsupplier.Country, supplier.Country);
+            Xunit.Assert.Equal(_newsupplier.ContactName, supplier.ContactName);
+            Xunit.Assert.Equal(_newsupplier.Phonenumber, supplier.Phonenumber);
+            Xunit.Assert.Equal(_newsupplier.Reference, supplier.Reference);
+
+            }
         [Fact, TestPriority(5)]
         public async Task PutSupplier()
         {
             var requestUri = "/api/v1/suppliers/1";
-            var response = await _client.PutAsync(requestUri, new StringContent("{\"id\": 1, \"code\": \"SUP0001\", \"name\": \"Bob, Parks and Johnson\", \"address\": \"5989 Sullivan Drives\", \"address_extra\": \"Apt. 996\", \"city\": \"Port Anitaburgh\", \"zip_code\": \"91688\", \"province\": \"Illinois\", \"country\": \"Czech Republic\", \"contact_name\": \"Toni Barnett\", \"phonenumber\": \"363.541.7282x36825\", \"reference\": \"LPaJ-SUP0001\"}", Encoding.UTF8, "application/json"));
+            var response = await _client.PutAsJsonAsync(requestUri, _supplierToPut);
             var result = await response.Content.ReadAsStringAsync();
             Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
@@ -66,7 +85,22 @@ namespace IntegrationTests
             var result = await response.Content.ReadAsStringAsync();
             Xunit.Assert.NotNull(result);
             Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Xunit.Assert.Contains("{\"id\":1,\"code\":\"SUP0001\",\"name\":\"Bob, Parks and Johnson\",\"address\":\"5989 Sullivan Drives\",\"address_Extra\":\"Apt. 996\",\"city\":\"Port Anitaburgh\",\"zip_Code\":\"91688\",\"province\":\"Illinois\",\"country\":\"Czech Republic\",\"contact_Name\":\"Toni Barnett\",\"phoneNumber\":\"363.541.7282x36825\",\"reference\":\"LPaJ-SUP0001\"", result);
+
+            Supplier? supplier = await response.Content.ReadFromJsonAsync<Supplier>();
+
+            Xunit.Assert.NotNull(supplier);
+            Xunit.Assert.Equal(_supplierToPut.Id, supplier.Id);
+            Xunit.Assert.Equal(_supplierToPut.Code, supplier.Code);
+            Xunit.Assert.Equal(_supplierToPut.Name, supplier.Name);
+            Xunit.Assert.Equal(_supplierToPut.Address, supplier.Address);
+            Xunit.Assert.Equal(_supplierToPut.AddressExtra, supplier.AddressExtra);
+            Xunit.Assert.Equal(_supplierToPut.City, supplier.City);
+            Xunit.Assert.Equal(_supplierToPut.ZipCode, supplier.ZipCode);
+            Xunit.Assert.Equal(_supplierToPut.Province, supplier.Province);
+            Xunit.Assert.Equal(_supplierToPut.Country, supplier.Country);
+            Xunit.Assert.Equal(_supplierToPut.ContactName, supplier.ContactName);
+            Xunit.Assert.Equal(_supplierToPut.Phonenumber, supplier.Phonenumber);
+            Xunit.Assert.Equal(_supplierToPut.Reference, supplier.Reference);
         }
         [Fact, TestPriority(7)]
         public async Task DeleteSupplier()
