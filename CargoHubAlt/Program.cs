4a4e4763ca16a81e9a814d1eb14f88d50528ce69
robin <robin.otterspeer2005@gmail.com>
2024-12-02 12:30:30 +0100

    logs now deleted in transfer

--- CargoHubAlt/Program.cs
+++ CargoHubAlt/Program.cs
@@ -25,9 +25,6 @@ public class Program
         .Filter.ByIncludingOnly(evt => evt.Properties["SourceContext"].ToString().Contains("OrderController"))
         .WriteTo.File("Logs/OrderController.log"))
     .WriteTo.Logger(lc => lc
-        .Filter.ByIncludingOnly(evt => evt.Properties["SourceContext"].ToString().Contains("TransferController"))
-        .WriteTo.File("Logs/TransferController.log"))
-    .WriteTo.Logger(lc => lc
         .Filter.ByIncludingOnly(evt => evt.Properties["SourceContext"].ToString().Contains("ClientController"))
         .WriteTo.File("Logs/ClientController.log"))
     .WriteTo.Logger(lc => lc
