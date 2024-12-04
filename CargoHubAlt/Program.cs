069617988c53ff5a106f13a14e8a386f3c3eed3a
Henri van der Stelt <hj.vdstelt@gmail.com>
2024-12-04 11:32:35 +0100

    Revert "V2 environment"
    This reverts commit 5a84056e513ff55b1d8921406302b4b0af0e86af.

--- CargoHubAlt/Program.cs
+++ CargoHubAlt/Program.cs
@@ -1,14 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using CargoHubAlt.Models;
+using CargoHubAlt.Interfaces;
 using CargoHubAlt.Services;
 using CargoHubAlt.Database;
 using Serilog;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using CargoHubAlt.Interfaces.InterfacesV1;
-using CargoHubAlt.Interfaces.InterfacesV2;
-using CargoHubAlt.Services.ServicesV1;
-using CargoHubAlt.Services.ServicesV2;
 
 public class Program
 {
@@ -16,7 +14,6 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // logging
         builder.Logging.ClearProviders();
         builder.Logging.AddConsole();
 
@@ -34,43 +31,27 @@ public class Program
 
         builder.Host.UseSerilog();
 
-        // V1
-        builder.Services.AddTransient<IClientServiceV1, ClientServiceV1>();
-        builder.Services.AddTransient<IInventoryServiceV1, InventoryServiceV1>();
-        builder.Services.AddTransient<IItemTypeServiceV1, ItemTypeServiceV1>();
-        builder.Services.AddTransient<IItemGroupServiceV1, ItemGroupServiceV1>();
-        builder.Services.AddTransient<IItemLineServiceV1, ItemLineServiceV1>();
-        builder.Services.AddTransient<IItemsServiceV1, ItemsServiceV1>();
-        builder.Services.AddTransient<IWarehouseServiceV1, WarehouseServiceV1>();
-        builder.Services.AddTransient<IShipmentServiceV1, ShipmentServiceV1>();
-        builder.Services.AddTransient<ITransferServiceV1, TransferServiceV1>();
-        builder.Services.AddTransient<ILocationServiceV1, LocationServiceV1>();
-        builder.Services.AddTransient<ISupplierServiceV1, SuppliersV1>();
-        builder.Services.AddTransient<IOrderServiceV1, OrderServiceV1>();
-        // V2
-        builder.Services.AddTransient<IClientServiceV2, ClientServiceV2>();
-        builder.Services.AddTransient<IInventoryServiceV2, InventoryServiceV2>();
-        builder.Services.AddTransient<IItemTypeServiceV2, ItemTypeServiceV2>();
-        builder.Services.AddTransient<IItemGroupServiceV2, ItemGroupServiceV2>();
-        builder.Services.AddTransient<IItemLineServiceV2, ItemLineServiceV2>();
-        builder.Services.AddTransient<IItemsServiceV2, ItemsServiceV2>();
-        builder.Services.AddTransient<IWarehouseServiceV2, WarehouseServiceV2>();
-        builder.Services.AddTransient<IShipmentServiceV2, ShipmentServiceV2>();
-        builder.Services.AddTransient<ITransferServiceV2, TransferServiceV2>();
-        builder.Services.AddTransient<ILocationServiceV2, LocationServiceV2>();
-        builder.Services.AddTransient<ISupplierServiceV2, SuppliersV2>();
-        builder.Services.AddTransient<IOrderServiceV2, OrderServiceV2>();
-
+        builder.Services.AddTransient<IClientService, ClientService>();
+        builder.Services.AddTransient<IInventoryService, InventoryService>();
+        builder.Services.AddTransient<IItemTypeService, ItemTypeService>();
+        builder.Services.AddTransient<IItemGroupService, ItemGroupService>();
+        builder.Services.AddTransient<IItemLineService, ItemLineService>();
+        builder.Services.AddTransient<IItemsService, ItemsService>();
+        builder.Services.AddTransient<IWarehouseService, WarehouseService>();
+        builder.Services.AddTransient<IShipmentService, ShipmentService>();
+        builder.Services.AddTransient<ITransferService, TransferService>();
+        builder.Services.AddTransient<ILocationService, LocationService>();
+        builder.Services.AddTransient<ISupplierService, Suppliers>();
+        builder.Services.AddTransient<IOrderService, OrderService>();
         builder.Services.AddScoped<ApiKeyActionFilter>();
         builder.Services.AddControllers();
-        builder.Services.AddControllers(options =>
-        {
+        builder.Services.AddControllers(options => {
             options.Filters.AddService<ApiKeyActionFilter>();
         });
         builder.Services.AddDbContext<CargoHubContext>(x => x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
         var app = builder.Build();
-
+        
 
         app.UseSerilogRequestLogging();
 
