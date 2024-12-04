64f00580b59854b6a56dd6c79cf049ecf3e28c5d
Henri van der Stelt <hj.vdstelt@gmail.com>
2024-12-04 13:22:05 +0100

    V2 environment

---
+++ CargoHubAlt/Controllers/ControllersV1/ItemGroupController.cs
@@ -0,0 +1,85 @@
+using Microsoft.AspNetCore.Mvc;
+using CargoHubAlt.Interfaces.InterfacesV1;
+using CargoHubAlt.Models;
+
+namespace CargoHubAlt.Controllers.ControllersV1
+{
+    [ApiController]
+    [Route("api/v1/item_groups")]
+    public class ItemGroupControllerV1 : Controller
+    {
+        readonly IItemGroupServiceV1 _itemsService;
+        public ItemGroupControllerV1(IItemGroupServiceV1 itemsservice)
+        {
+            this._itemsService = itemsservice;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetAllItemGroups()
+        {
+            return Ok(await this._itemsService.GetAllItemGroup());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOneItemGroup([FromRoute] int id)
+        {
+            ItemGroup? toReturn = await this._itemsService.FindItemGroup(id);
+
+            // if (toReturn is null) return Ok("null");
+            // return Ok(toReturn);
+
+            if (toReturn is null) return NotFound($"ID {id} not found");
+            else return Ok(toReturn);
+
+        }
+
+        [HttpGet("{id}/items")]
+        public async Task<IActionResult> GetItemsfromItemGroupById([FromRoute] int id)
+        {
+            if (id < 0) return BadRequest("invalid id");
+            var success = await this._itemsService.GetItemsfromItemGroupById(id);
+            if (success is null || success.Count() == 0) return NotFound($"Item Group with ID: {id} not found");
+            else return Ok(success);
+        }
+
+
+        [HttpPost()]
+        public async Task<IActionResult> AddItemGroup([FromBody] ItemGroup? toAdd)
+        {
+            if (toAdd is null) return BadRequest("this is not an item group");
+            int? success = await this._itemsService.AddItemGroup(toAdd);
+            if (success is not null) return Created("api/v1/item_groups", toAdd.Id);
+            else return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutItemGroup([FromRoute] int id, [FromBody] ItemGroup toupdateto)
+        {
+
+            ItemGroup? success = await this._itemsService.UpdateItemGroup(id, toupdateto);
+
+            // return Ok();
+
+            if (success is null) return NotFound($"Id not Found: {id}");
+            return Ok(success);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItemgroup([FromRoute] int id)
+        {
+
+            ItemGroup? success = await this._itemsService.DeleteItemGroup(id);
+
+            // return Ok();
+
+            if (success is null) return NotFound($"ID not found: {id}");
+            else return Ok(success);
+        }
+        [HttpPost("load/{path}")]
+        public async Task<IActionResult> LoadLocations([FromRoute] string path)
+        {
+            await _itemsService.LoadFromJson(path);
+            return Ok();
+        }
+    }
+}
---
+++ CargoHubAlt/Services/ServicesV2/ItemsService.cs
@@ -0,0 +1,128 @@
+using Microsoft.EntityFrameworkCore;
+using CargoHubAlt.Models;
+using CargoHubAlt.Database;
+using CargoHubAlt.Interfaces.InterfacesV2;
+using System.Text.Json;
+
+namespace CargoHubAlt.Services.ServicesV2
+{
+    public class ItemsServiceV2 : IItemsServiceV2
+    {
+        readonly CargoHubContext _context;
+        public ItemsServiceV2(CargoHubContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Item>> GetItems()
+        {
+            return await this._context.Items.ToListAsync();
+        }
+
+        public async Task<Item?> GetItem(string id)
+        {
+            return await this._context.Items.FirstOrDefaultAsync(_ => _.Uid == id);
+        }
+        public async Task<List<Inventory>> GetInventoryByItem(string id)
+        {
+            return await this._context.Inventories.Where(_ => _.ItemId == id).ToListAsync();
+        }
+
+        public async Task<Dictionary<string, int>> GetInventoryTotalsByItem(string id)
+        {
+            Dictionary<string, int> toReturn = new Dictionary<string, int>();
+            toReturn.Add("total_expected", 0);
+            toReturn.Add("total_ordered", 0);
+            toReturn.Add("total_allocated", 0);
+            toReturn.Add("total_available", 0);
+            foreach (Inventory inv in await this._context.Inventories.Where(_ => _.ItemId == id).ToListAsync())
+            {
+                toReturn["total_expected"] += inv.TotalExpected;
+                toReturn["total_ordered"] += inv.TotalOrdered;
+                toReturn["total_allocated"] += inv.TotalAllocated;
+                toReturn["total_available"] += inv.TotalAvailable;
+            }
+            return toReturn;
+        }
+        public async Task<string?> AddItem(Item? toAdd)
+        {
+            if (toAdd is null) return null;
+            if (await GetItem(toAdd.Uid) != null) return "Existed";
+            await this._context.Items.AddAsync(toAdd);
+            if (await this._context.SaveChangesAsync() >= 1) return toAdd.Uid;
+            else return null;
+        }
+
+        public async Task<Item?> UpdateItem(string toUpdate, Item UpdateTo)
+        {
+            Item? found = await this._context.Items.FindAsync(toUpdate);
+            if (found is null) return null;
+            found.Uid = UpdateTo.Uid;
+            found.Code = UpdateTo.Code;
+            found.Description = UpdateTo.Description;
+            found.ShortDescription = UpdateTo.ShortDescription;
+            found.UpcCode = UpdateTo.UpcCode;
+            found.ModelNumber = UpdateTo.ModelNumber;
+            found.CommodityCode = UpdateTo.CommodityCode;
+            found.ItemLine = UpdateTo.ItemLine;
+            found.ItemGroup = UpdateTo.ItemGroup;
+            found.ItemType = UpdateTo.ItemType;
+            found.UnitPurchaseQuantity = UpdateTo.UnitPurchaseQuantity;
+            found.UnitOrderQuantity = UpdateTo.UnitOrderQuantity;
+            found.PackOrderQuantity = UpdateTo.PackOrderQuantity;
+            found.SupplierId = UpdateTo.SupplierId;
+            found.SupplierCode = UpdateTo.SupplierCode;
+            found.SupplierPartNumber = UpdateTo.SupplierPartNumber;
+            found.UpdatedAt = UpdateTo.UpdatedAt;
+
+
+            if (await this._context.SaveChangesAsync() >= 1) return found;
+            else return null;
+        }
+
+        public async Task<Item?> RemoveItem(string toRemove)
+        {
+            Item? found = await this._context.Items.FindAsync(toRemove);
+            if (found is null) return null;
+            this._context.Items.Remove(found);
+            if (await this._context.SaveChangesAsync() >= 1) return found;
+            else return null;
+        }
+
+        public async Task LoadFromJson(string path)
+        {
+            path = "data/" + path;
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                List<Item>? items = JsonSerializer.Deserialize<List<Item>>(json);
+                if (items == null)
+                {
+                    return;
+                }
+                foreach (Item item in items)
+                {
+                    await SaveToDatabase(item);
+                }
+            }
+        }
+        public async Task<string> SaveToDatabase(Item item)
+        {
+            if (item is null)
+            {
+                return "Please provide an item to add.";
+            }
+            if (item.Code == null) { item.Code = "N/A"; }
+            if (item.Description == null) { item.Description = "N/A"; }
+            if (item.ShortDescription == null) { item.ShortDescription = "N/A"; }
+            if (item.UpcCode == null) { item.UpcCode = "N/A"; }
+            if (item.ModelNumber == null) { item.ModelNumber = "N/A"; }
+            if (item.CommodityCode == null) { item.CommodityCode = "N/A"; }
+            if (item.SupplierCode == null) { item.SupplierCode = "N/A"; }
+            if (item.SupplierPartNumber == null) { item.SupplierPartNumber = "N/A"; }
+            await _context.Items.AddAsync(item);
+            await _context.SaveChangesAsync();
+            return item.Uid;
+        }
+    }
+}
