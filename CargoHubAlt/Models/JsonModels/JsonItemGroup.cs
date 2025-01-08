955a4b403f8ce8c187b455f9b8443bce2705efa6
Joshuaburg2004 <joshuaburg2004@hotmail.nl>
2025-01-08 14:48:32 +0100

    ItemGroup conversion speed up done

---
+++ CargoHubAlt/Models/JsonModels/JsonItemGroup.cs
@@ -0,0 +1,23 @@
+using CargoHubAlt.Database;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using CargoHubAlt.Models;
+namespace CargoHubAlt.JsonModels{
+    public class JsonItemGroup : JsonBase{
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? description { get; set; }
+        public string created_at { get; set; } = GetTimeStamp();
+        public string updated_at { get; set; } = GetTimeStamp();
+
+        public ItemGroup ToItemGroup(){
+            return new ItemGroup(){
+                Id = id,
+                Name = name,
+                Description = description,
+                CreatedAt = created_at,
+                UpdatedAt = updated_at
+            };
+        }
+    }
+}
