81285ec9bef798188623e9a11d90c5f7bb6310ab
Robin <robin.otterspeer2005@gmail.com>
2025-01-15 13:47:18 +0100

    item group done

--- IntegrationTests/IntegrationTestsV2/ItemGroupTest.cs
+++ IntegrationTests/IntegrationTestsV2/ItemGroupTest.cs
@@ -14,6 +14,7 @@ namespace IntegrationTests;
 
 public class ItemGroupTest : BaseTest
 {
+    var requestUri = "/api/v1/item_groups";
     private ItemGroup _itemGroupCreate = new ItemGroup(1, "Laptop", "Never gonna give you up");
     private ItemGroup _itemGroupPut = new ItemGroup(1, "Laptop", "Never gonna let you down");
     private Item _item = new Item("P000005", "Laptop", "Never gonna give you up", "Never gonna", "123456789", "123456789", "123456789", 1, 1, 1, 1, 1, 1, 1, "123456789", "123456789");
@@ -23,7 +24,6 @@ public class ItemGroupTest : BaseTest
     [Fact, TestPriority(0)]
     public async Task CreateItemGroup()
     {
-        var requestUri = "/api/v1/item_groups";
         var response = await _client.PostAsJsonAsync(requestUri, _itemGroupCreate);
         Xunit.Assert.Equal(HttpStatusCode.Created, response.StatusCode);
     }
@@ -31,15 +31,13 @@ public class ItemGroupTest : BaseTest
     [Fact, TestPriority(1)]
     public async Task CreateItem()
     {
-        var requestUri = "/api/v1/items";
-        var response = await _client.PostAsJsonAsync(requestUri, _item);
+        var response = await _client.PostAsJsonAsync($"{requestUri}/items", _item);
         Xunit.Assert.Equal(HttpStatusCode.Created, response.StatusCode);
     }
 
     [Fact, TestPriority(2)]
     public async Task GetAllItemGroupsOne()
     {
-        var requestUri = "/api/v1/item_groups";
         var response = await _client.GetAsync(requestUri);
         var result = await response.Content.ReadAsStringAsync();
         Xunit.Assert.NotNull(response);
@@ -55,8 +53,7 @@ public class ItemGroupTest : BaseTest
     [Fact, TestPriority(3)]
     public async Task GetItemGroup()
     {
-        var requestUri = "/api/v1/item_groups/1";
-        var response = await _client.GetAsync(requestUri);
+        var response = await _client.GetAsync($"{requestUri}/1");
         var result = await response.Content.ReadAsStringAsync();
         Xunit.Assert.NotNull(response);
         Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
@@ -71,8 +68,7 @@ public class ItemGroupTest : BaseTest
     [Fact, TestPriority(4)]
     public async Task GetItemGroupNotFound()
     {
-        var requestUri = "/api/v1/item_groups/3";
-        var response = await _client.GetAsync(requestUri);
+        var response = await _client.GetAsync($"{requestUri}/3");
         var result = await response.Content.ReadAsStringAsync();
         Xunit.Assert.NotNull(response);
         Xunit.Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
@@ -82,8 +78,7 @@ public class ItemGroupTest : BaseTest
     public async Task UpdateItemGroup()
     {
         // Description is updated from "Never gonna give you up" to "Never gonna let you down"
-        var requestUri = "/api/v1/item_groups/1";
-        var response = await _client.PutAsJsonAsync(requestUri, _itemGroupPut);
+        var response = await _client.PutAsJsonAsync($"{requestUri}/1", _itemGroupPut);
         var result = await response.Content.ReadAsStringAsync();
         Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
@@ -91,8 +86,7 @@ public class ItemGroupTest : BaseTest
     [Fact, TestPriority(6)]
     public async Task GetUpdatedItemGroup()
     {
-        var requestUri = "/api/v1/item_groups/1";
-        var response = await _client.GetAsync(requestUri);
+        var response = await _client.GetAsync($"{requestUri}/1");
         var result = await response.Content.ReadAsStringAsync();
         Xunit.Assert.NotNull(response);
         Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
@@ -106,8 +100,7 @@ public class ItemGroupTest : BaseTest
     [Fact, TestPriority(7)]
     public async Task GetItemGroupItemsNotFound()
     {
-        var requestUri = "/api/v1/item_groups/3/items";
-        var response = await _client.GetAsync(requestUri);
+        var response = await _client.GetAsync($"{requestUri}/3/items");
         var result = await response.Content.ReadAsStringAsync();
         Xunit.Assert.NotNull(response);
         Xunit.Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
@@ -115,8 +108,7 @@ public class ItemGroupTest : BaseTest
     [Fact, TestPriority(8)]
     public async Task GetItemGroupItems()
     {
-        var requestUri = "/api/v1/item_groups/1/items";
-        var response = await _client.GetAsync(requestUri);
+        var response = await _client.GetAsync($"{requestUri}/1/items");
         var result = await response.Content.ReadAsStringAsync();
         Xunit.Assert.NotNull(response);
         Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
@@ -144,8 +136,7 @@ public class ItemGroupTest : BaseTest
     [Fact, TestPriority(9)]
     public async Task GetItemGroupNotFoundAgain()
     {
-        var requestUri = "/api/v1/item_groups/2";
-        var response = await _client.GetAsync(requestUri);
+        var response = await _client.GetAsync($"{requestUri}/2");
         var result = await response.Content.ReadAsStringAsync();
         Xunit.Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
     }
@@ -153,15 +144,13 @@ public class ItemGroupTest : BaseTest
     [Fact, TestPriority(10)]
     public async Task DeleteItemGroup()
     {
-        var requestUri = "/api/v1/item_groups/1";
-        var response = await _client.DeleteAsync(requestUri);
+        var response = await _client.DeleteAsync($"{requestUri}/1");
         Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
 
     [Fact, TestPriority(11)]
     public async Task GetItemGroupEmpty()
     {
-        var requestUri = "/api/v1/item_groups";
         var response = await _client.GetAsync(requestUri);
         var result = await response.Content.ReadAsStringAsync();
         Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
@@ -170,8 +159,7 @@ public class ItemGroupTest : BaseTest
     [Fact, TestPriority(12)]
     public async Task DeleteItem()
     {
-        var requestUri = "/api/v1/items/P000005";
-        var response = await _client.DeleteAsync(requestUri);
+        var response = await _client.DeleteAsync($"{requestUri}/P000005");
         Xunit.Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
 }
