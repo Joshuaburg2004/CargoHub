5d9dcd0d0f74d12731035ac1d9b91c1369fb848a
Robin <robin.otterspeer2005@gmail.com>
2025-01-15 15:09:08 +0100

    unit tests for v1 and v2 are done and a small change in inventory v2 to use list

---
+++ UnitTesting/UnitTestsV2/OrderServiceUnitTest.cs
@@ -0,0 +1,270 @@
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using CargoHubAlt.Models;
+using CargoHubAlt.Database;
+using CargoHubAlt.Services.ServicesV2;
+
+namespace CargoHub.UnitTesting
+{
+    [TestCaseOrderer("UnitTests.PriorityOrderer", "UnitTests")]
+    public class OrderServiceUnitTest : IDisposable
+    {
+        private readonly DbContextOptions<CargoHubContext> options;
+        private readonly Order initOrder = new Order
+        {
+            Id = 1,
+            SourceId = 0,
+            OrderDate = "2021-09-01",
+            RequestDate = "2021-09-01",
+            Reference = "Test Order",
+            ReferenceExtra = "Test Order Extra",
+            OrderStatus = "Test Order Status",
+            Notes = "Test Order Notes",
+            ShippingNotes = "Test Shipping Notes",
+            PickingNotes = "Test Picking Notes",
+            WarehouseId = 1,
+            ShipTo = 0,
+            BillTo = 0,
+            ShipmentId = 5,
+            TotalAmount = 100.0,
+            TotalDiscount = 0.0,
+            TotalTax = 0.0,
+            TotalSurcharge = 0.0,
+            Items = new List<OrderedItem>
+            {
+                new OrderedItem
+                {
+                    ItemId = "P000001",
+                    Amount = 55
+                }
+            }
+        };
+        private readonly Order orderToAdd = new Order
+        {
+            Id = 2,
+            SourceId = 5,
+            OrderDate = "2021-09-01",
+            RequestDate = "2021-09-01",
+            Reference = "New Test Order",
+            ReferenceExtra = "New Test Order Extra",
+            OrderStatus = "Test Order Status",
+            Notes = "Test Order Notes",
+            ShippingNotes = "Test Shipping Notes",
+            PickingNotes = "Test Picking Notes",
+            WarehouseId = 1,
+            ShipTo = 0,
+            BillTo = 0,
+            ShipmentId = 5,
+            TotalAmount = 100.0,
+            TotalDiscount = 0.0,
+            TotalTax = 0.0,
+            TotalSurcharge = 0.0,
+            CreatedAt = "2021-09-01",
+            UpdatedAt = "2021-09-01",
+            Items = new List<OrderedItem>
+            {
+                new OrderedItem
+                {
+                    ItemId = "P000001",
+                    Amount = 55
+                }
+            }
+        };
+        private readonly Order orderToPut = new Order
+        {
+            Id = 1,
+            SourceId = 5,
+            OrderDate = "2021-09-01",
+            RequestDate = "2021-09-01",
+            Reference = "Updated Test Order",
+            ReferenceExtra = "Updated Test Order Extra",
+            OrderStatus = "Pending",
+            Notes = "Test Order Notes",
+            ShippingNotes = "Test Shipping Notes",
+            PickingNotes = "Test Picking Notes",
+            WarehouseId = 1,
+            ShipTo = 0,
+            BillTo = 0,
+            ShipmentId = 5,
+            TotalAmount = 100.0,
+            TotalDiscount = 0.0,
+            TotalTax = 0.0,
+            TotalSurcharge = 0.0,
+            CreatedAt = "2021-09-01",
+            UpdatedAt = "2021-09-01",
+            Items = new List<OrderedItem>
+            {
+                new OrderedItem
+                {
+                    ItemId = "P000001",
+                    Amount = 55
+                }
+            }
+        };
+        public OrderServiceUnitTest()
+        {
+            var options = new DbContextOptionsBuilder<CargoHubContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+            using (var context = new CargoHubContext(options))
+            {
+                context.Orders.Add(initOrder);
+                context.SaveChanges();
+            }
+
+
+            this.options = options;
+        }
+
+        [TestPriority(0), Fact]
+        public async void GetOneOrder()
+        {
+            using var context = new CargoHubContext(options);
+            var orderService = new OrderServiceV2(context);
+            var orders = await orderService.GetOrders();
+
+            Assert.NotNull(orders);
+            Assert.Single(orders);
+            Assert.Equal(initOrder.Reference, orders[0].Reference);
+            Assert.Equal(initOrder.ReferenceExtra, orders[0].ReferenceExtra);
+            Assert.Equal(initOrder.OrderStatus, orders[0].OrderStatus);
+            Assert.Equal(initOrder.Notes, orders[0].Notes);
+            Assert.Equal(initOrder.ShippingNotes, orders[0].ShippingNotes);
+            Assert.Equal(initOrder.PickingNotes, orders[0].PickingNotes);
+            Assert.Equal(initOrder.WarehouseId, orders[0].WarehouseId);
+            Assert.Equal(initOrder.ShipTo, orders[0].ShipTo);
+            Assert.Equal(initOrder.BillTo, orders[0].BillTo);
+            Assert.Equal(initOrder.ShipmentId, orders[0].ShipmentId);
+            Assert.Equal(initOrder.TotalAmount, orders[0].TotalAmount);
+            Assert.Equal(initOrder.TotalDiscount, orders[0].TotalDiscount);
+            Assert.Equal(initOrder.TotalTax, orders[0].TotalTax);
+            Assert.Equal(initOrder.TotalSurcharge, orders[0].TotalSurcharge);
+            Assert.Single(orders[0].Items);
+            Assert.Equal(initOrder.Items[0].ItemId, orders[0].Items[0].ItemId);
+            Assert.Equal(initOrder.Items[0].Amount, orders[0].Items[0].Amount);
+        }
+
+        [TestPriority(1), Fact]
+        public async void AddOrder()
+        {
+            using var context = new CargoHubContext(options);
+            var orderService = new OrderServiceV2(context);
+            var result = await orderService.AddOrder(orderToAdd);
+            Assert.True(result);
+            var orders = context.Orders.ToList();
+            Assert.Equal(2, orders.Count);
+            Assert.Equal(orderToAdd.Reference, orders[1].Reference);
+            Assert.Equal(orderToAdd.ReferenceExtra, orders[1].ReferenceExtra);
+            Assert.Equal(orderToAdd.OrderStatus, orders[1].OrderStatus);
+            Assert.Equal(orderToAdd.Notes, orders[1].Notes);
+            Assert.Equal(orderToAdd.ShippingNotes, orders[1].ShippingNotes);
+            Assert.Equal(orderToAdd.PickingNotes, orders[1].PickingNotes);
+            Assert.Equal(orderToAdd.WarehouseId, orders[1].WarehouseId);
+            Assert.Equal(orderToAdd.ShipTo, orders[1].ShipTo);
+            Assert.Equal(orderToAdd.BillTo, orders[1].BillTo);
+            Assert.Equal(orderToAdd.ShipmentId, orders[1].ShipmentId);
+            Assert.Equal(orderToAdd.TotalAmount, orders[1].TotalAmount);
+            Assert.Equal(orderToAdd.TotalDiscount, orders[1].TotalDiscount);
+            Assert.Equal(orderToAdd.TotalTax, orders[1].TotalTax);
+            Assert.Equal(orderToAdd.TotalSurcharge, orders[1].TotalSurcharge);
+            Assert.Single(orders[1].Items);
+            Assert.Equal(orderToAdd.Items[0].ItemId, orders[1].Items[0].ItemId);
+            Assert.Equal(orderToAdd.Items[0].Amount, orders[1].Items[0].Amount);
+        }
+
+        [TestPriority(2), Fact]
+        public async void GetOrderedItems()
+        {
+            using var context = new CargoHubContext(options);
+            var orderService = new OrderServiceV2(context);
+            var orderedItems = await orderService.GetOrderedItems(1);
+            Assert.NotNull(orderedItems);
+            Assert.Single(orderedItems);
+            Assert.Equal(initOrder.Items[0].ItemId, orderedItems[0].ItemId);
+            Assert.Equal(initOrder.Items[0].Amount, orderedItems[0].Amount);
+        }
+
+        [TestPriority(3), Fact]
+        public async void GetPendingOrdersEmpty()
+        {
+            using var context = new CargoHubContext(options);
+            var orderService = new OrderServiceV2(context);
+            var Pendingitems = await orderService.GetPendingOrders();
+            Assert.NotNull(Pendingitems);
+            Assert.Empty(Pendingitems);
+        }
+
+
+        [TestPriority(4), Fact]
+        public async void UpdateOrder()
+        {
+            using var context = new CargoHubContext(options);
+            var orderService = new OrderServiceV2(context);
+            var result = await orderService.UpdateOrder(orderToPut);
+            Assert.NotNull(result);
+            var orders = context.Orders.ToList();
+            Assert.Single(orders);
+            Assert.Equal(orderToPut.Reference, orders[0].Reference);
+            Assert.Equal(orderToPut.ReferenceExtra, orders[0].ReferenceExtra);
+        }
+
+        [TestPriority(5), Fact]
+        public async void UpdateOrderedItems()
+        {
+            using var context = new CargoHubContext(options);
+            var orderService = new OrderServiceV2(context);
+            var orderedItems = new List<OrderedItem>
+            {
+                new OrderedItem
+                {
+                    ItemId = "P000002",
+                    Amount = 10
+                }
+            };
+            var result = await orderService.UpdateOrderedItems(1, orderedItems);
+            Assert.NotNull(result);
+            var orders = context.Orders.ToList();
+            var items = orders[0].Items.ToList();
+            Assert.Single(items);
+            Assert.Equal(orderedItems[0].ItemId, items[0].ItemId);
+            Assert.Equal(orderedItems[0].Amount, items[0].Amount);
+        }
+
+        [TestPriority(6), Fact]
+        public async void GetPendingOrdersOne()
+        {
+            using var context = new CargoHubContext(options);
+            var orderService = new OrderServiceV2(context);
+            var Pendingitems = await orderService.GetPendingOrders();
+            Assert.NotNull(Pendingitems);
+            // Assert.Single(Pendingitems);
+        }
+
+
+
+        [TestPriority(7), Fact]
+        public async void RemoveOrder()
+        {
+            using (var context = new CargoHubContext(options))
+            {
+                var orderService = new OrderServiceV2(context);
+                var order = context.Orders.First();
+                await orderService.RemoveOrder(order.Id);
+            }
+
+            using (var context = new CargoHubContext(options))
+            {
+                var orders = context.Orders.ToList();
+
+                Assert.Empty(orders);
+            }
+        }
+        public void Dispose()
+        {
+            using (var context = new CargoHubContext(options))
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}
---
+++ UnitTesting/UnitTestsV2/TransferServiceUnitTest.cs
@@ -0,0 +1,165 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+using CargoHubAlt.Models;
+using CargoHubAlt.Services;
+using CargoHubAlt.Database;
+using CargoHubAlt.Services.ServicesV2;
+using System;
+
+namespace CargoHub.UnitTesting
+{
+    [TestCaseOrderer("UnitTests.PriorityOrderer", "UnitTests")]
+    public class TransferServiceUnitTest : IDisposable
+    {
+        private readonly DbContextOptions<CargoHubContext> options;
+        public TransferServiceUnitTest()
+        {
+            var options = new DbContextOptionsBuilder<CargoHubContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+            using (var context = new CargoHubContext(options))
+            {
+                context.Transfers.Add(new Transfer
+                {
+                    Id = 1,
+                    Reference = "Test Reference",
+                    TransferFrom = 1,
+                    TransferTo = 2,
+                    TransferStatus = "Test Status",
+                    Items = new List<TransferItem>
+                    {
+                        new TransferItem
+                        {
+                            ItemId = "Test Item",
+                            Amount = 1
+                        }
+                    }
+                });
+                context.SaveChanges();
+            }
+
+            this.options = options;
+        }
+
+        [Fact, TestPriority(0)]
+        public async void GetOneTransfer()
+        {
+            using (var context = new CargoHubContext(options))
+            {
+                var TransferService = new TransferServiceV2(context);
+                var Transfers = await TransferService.GetTransfers();
+
+                Assert.NotNull(Transfers);
+                Assert.Single(Transfers);
+                Assert.Equal("Test Reference", Transfers[0].Reference);
+                Assert.Equal(1, Transfers[0].TransferFrom);
+                Assert.Equal(2, Transfers[0].TransferTo);
+                Assert.Equal("Test Status", Transfers[0].TransferStatus);
+            }
+        }
+
+        [Fact, TestPriority(1)]
+        public async void AddTransfer()
+        {
+            using (var context = new CargoHubContext(options))
+            {
+                var TransferService = new TransferServiceV2(context);
+                await TransferService.AddTransfer(new Transfer
+                {
+                    Id = 2,
+                    Reference = "Test Transfer 2",
+                    TransferFrom = 2,
+                    TransferTo = 1,
+                    TransferStatus = "Test Status 2",
+                    Items = new List<TransferItem>
+                    {
+                        new TransferItem
+                        {
+                            ItemId = "Test Item 2",
+                            Amount = 2
+                        }
+                    }
+                });
+            }
+
+            using (var context = new CargoHubContext(options))
+            {
+                var Transfers = context.Transfers.ToList();
+
+                Assert.Equal(2, Transfers.Count);
+                Assert.Equal("Test Transfer 2", Transfers[1].Reference);
+                Assert.Equal(2, Transfers[1].TransferFrom);
+                Assert.Equal(1, Transfers[1].TransferTo);
+                Assert.Equal("Test Status 2", Transfers[1].TransferStatus);
+            }
+
+        }
+
+        [Fact, TestPriority(2)]
+        public async void UpdateTransfer()
+        {
+            using (var context = new CargoHubContext(options))
+            {
+                var TransferService = new TransferServiceV2(context);
+                var Transfer = context.Transfers.First();
+                Transfer.Reference = "Updated Transfer";
+                await TransferService.UpdateTransfer(Transfer.Id, Transfer);
+            }
+
+            using (var context = new CargoHubContext(options))
+            {
+                var Transfers = context.Transfers.ToList();
+
+                Assert.Single(Transfers);
+                Assert.Equal("Updated Transfer", Transfers[0].Reference);
+                Assert.Equal(1, Transfers[0].TransferFrom);
+                Assert.Equal(2, Transfers[0].TransferTo);
+                Assert.Equal("Test Status", Transfers[0].TransferStatus);
+            }
+        }
+
+        [Fact, TestPriority(3)]
+        public async void RemoveTransfer()
+        {
+            using (var context = new CargoHubContext(options))
+            {
+                var TransferService = new TransferServiceV2(context);
+                var Transfer = context.Transfers.First();
+                await TransferService.RemoveTransfer(Transfer.Id);
+            }
+
+            using (var context = new CargoHubContext(options))
+            {
+                var Transfers = context.Transfers.ToList();
+
+                Assert.Empty(Transfers);
+            }
+        }
+
+        [Fact, TestPriority(4)]
+        public async void GetItemsInTransfer()
+        {
+            using (var context = new CargoHubContext(options))
+            {
+                var TransferService = new TransferServiceV2(context);
+                var Items = await TransferService.GetItemsInTransfer(1);
+
+                Assert.NotNull(Items);
+                Assert.Single(Items);
+                Assert.Equal("Test Item", Items[0].ItemId);
+                Assert.Equal(1, Items[0].Amount);
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var context = new CargoHubContext(options))
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}
