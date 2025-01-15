8c4666d745c2b99140e46295feca807d80f41374
Joshuaburg2004 <joshuaburg2004@hotmail.nl>
2025-01-15 09:59:59 +0100

    Add a bit of the AnalyticService

--- CargoHubAlt/Interfaces/InterfacesV2/IAnalyticsService.cs
+++ CargoHubAlt/Interfaces/InterfacesV2/IAnalyticsService.cs
@@ -2,6 +2,6 @@ using CargoHubAlt.Models;
 
 namespace CargoHubAlt.Interfaces.InterfacesV2{
     public interface IAnalyticsService{
-        public Task<Dictionary<string, Dictionary<int, Item>>> GetAnalytics(DateOnly? FromDate, DateOnly? ToDate);
+        public Task<Dictionary<string, Dictionary<int, Item>>> GetAnalytics(DateOnly? FromDate = null, DateOnly? ToDate = null);
     }
 }
