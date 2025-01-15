fd6999aac81e1e067563c2a509e360fca3ee904a
Joshuaburg2004 <joshuaburg2004@hotmail.nl>
2025-01-15 11:26:40 +0100

    Finished analytics based on the recommendations of the PO

--- CargoHubAlt/Interfaces/InterfacesV2/IAnalyticsService.cs
+++ CargoHubAlt/Interfaces/InterfacesV2/IAnalyticsService.cs
@@ -2,6 +2,6 @@ using CargoHubAlt.Models;
 
 namespace CargoHubAlt.Interfaces.InterfacesV2{
     public interface IAnalyticsService{
-        public Task<Dictionary<string, Dictionary<int, Item>>> GetAnalytics(DateOnly? FromDate = null, DateOnly? ToDate = null);
+        public Task<Analytic> GetAnalytics(DateOnly? FromDate = null, DateOnly? ToDate = null);
     }
 }
